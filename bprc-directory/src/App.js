9b19b7edcffbdf8677a387505283d233e541eaf9
Torshawna Chew <71048113+toch6278@users.noreply.github.com>
2024-01-19 11:28:49 -0700

    create react app

---
+++ bprc-directory/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> BPRC Resource Directory</p>
+        <p> Access all available resources through the Boulder Pregnancy Rescource Center</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
