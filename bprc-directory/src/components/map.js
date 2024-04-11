54292f3fbe211dd6c1a7c5bfca49d2e04872777c
Torshawna Chew <71048113+toch6278@users.noreply.github.com>
2024-04-11 10:35:15 -0600

    trying to commit

--- bprc-directory/src/components/map.js
+++ bprc-directory/src/components/map.js
@@ -1,43 +1,50 @@
-import * as React from 'react'
-import { useState, useMemo, useCallback, useRef } from 'react';
-import { GoogleMap, Marker, DirectionsRenderer, Circle, MarkerClusterer} from "@react-google-maps/api";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import * as React from 'react'
+// import { useState, useMemo, useCallback, useRef } from 'react';
+// import { GoogleMap, Marker, DirectionsRenderer, Circle, MarkerClusterer} from "@react-google-maps/api";
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Source: https://www.youtube.com/watch?v=6mFaeJ4JfVM 
+// // Source: https://www.youtube.com/watch?v=6mFaeJ4JfVM 
 
-export default class Map extends React.Component {
+// export default class Map extends React.Component {
 
-    // header = () =? {
-    //     return {
+//     // header = () =? {
+//     //     return {
 
-    //     }
-    // }
-    // map = () => {
-    //     return {
-    //         <div className = "map"> Map </div>
-    //     }
-    // }
+//     //     }
+//     // }
+//     // map = () => {
+//     //     return {
+//     //         <div className = "map"> Map </div>
+//     //     }
+//     // }
 
-<<<<<<< HEAD
-  return (
-    <LoadScript googleMapsApiKey={apiKey}>
-      <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={4}>
-        {locations.map((location) => (
-          <Marker key={location.id} position={{ lat: location.lat, lng: location.lng }} />
-        ))}
-      </GoogleMap>
-    </LoadScript>
-  );
+// <<<<<<< HEAD
+// <<<<<<< HEAD
+//   return (
+//     <LoadScript googleMapsApiKey={apiKey}>
+//       <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={4}>
+//         {locations.map((location) => (
+//           <Marker key={location.id} position={{ lat: location.lat, lng: location.lng }} />
+//         ))}
+//       </GoogleMap>
+//     </LoadScript>
+//   );
 
-};
+// };
 
-export default Map;
-=======
-    // render()
-    // {
-        // return (
+// export default Map;
+// =======
+// =======
+// >>>>>>> parent of ee661e7 (fix clearing inputs after inputted)
+//     // render()
+//     // {
+//         // return (
             
-        // )
-    // }
-}
->>>>>>> parent of ee661e7 (fix clearing inputs after inputted)
+//         // )
+//     // }
+// <<<<<<< HEAD
+// }
+// >>>>>>> parent of ee661e7 (fix clearing inputs after inputted)
+// =======
+// }
+// >>>>>>> parent of ee661e7 (fix clearing inputs after inputted)
